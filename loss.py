42939cfbd6618d4958fce730a93c17288e6b5b70
WILeroy <wileroy@163.com>
2020-04-07 22:46:31 +0800

    update

--- loss.py
+++ loss.py
@@ -4,58 +4,49 @@ from __future__ import print_function
 
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.utils import to_categorical
 
-def softmax_cross_entropy(labels, logits, n_classes, weights=None):
-    """ Computes cross entropy between labels and logits.
+def categorical_cross_entropy(n_classes, weights=None):
 
-    Args:
-        labels: an array of numpy or a 'Tensor', shape = [?, W, H, n_classes].
-        logits: output of the model, no softmax, shape = [?, W, H, n_classes].
-        weights: Node, an array of numpy or a tensor, shape = [n_classes].
-    
-    Returns:
-        The value of the weighted cross entropy.
-    """
+    if weights is None:
+        weights = tf.ones([n_classes], dtype=tf.float32)
+    else:
+        weights = tf.cast(weights, dtype=tf.float32)
 
-    with tf.name_scope('loss'):
-        if weights is None:
-            weights = tf.ones([n_classes], dtype=tf.float32)
-        else:
-            weights = tf.cast(weights, dtype=tf.float32)
+    def _categorical_cross_entropy(labels, logits):
+        """ Computes cross entropy between labels and logits.
+        Args:
+            labels: an array of numpy or a 'Tensor', shape = [?, W, H, n_classes].
+            logits: output of the model by softmax, shape = [?, W, H, n_classes].
+            weights: None, an array of numpy or a 'Tensor', shape = [n_classes].
+
+        Returns:
+            The value of the weighted cross entropy.
+        """
 
         flat_labels = tf.cast(tf.reshape(labels, [-1, n_classes]), dtype=tf.float32)
         flat_logits = tf.cast(tf.reshape(logits, [-1, n_classes]), dtype=tf.float32)
 
-        epsilon = tf.constant(value=1e-7)
-        logits_softmax = tf.nn.softmax(flat_logits) + epsilon
+        weight_cross_entropy = -tf.reduce_sum(
+            tf.multiply(flat_labels*tf.math.log(flat_logits), weights),
+            axis=1)
 
-        weight_cross_entropy = -tf.reduce_sum(tf.multiply(flat_labels*tf.log(logits_softmax), weights),
-                                              axis=1)
+        return tf.reduce_mean(weight_cross_entropy)
 
-    return tf.reduce_mean(weight_cross_entropy)
+    return _categorical_cross_entropy
 
-def softmax_focal_loss(labels, logits, n_classes, gamma=2., alpha=.25):
-    """ Computes focal loss between labels and logits.
-    """
-
-    with tf.name_scope('loss'):
-        flat_labels = tf.cast(tf.reshape(labels, [-1, n_classes]), dtype=tf.float32)
-        flat_logits = tf.cast(tf.reshape(logits, [-1, n_classes]), dtype=tf.float32)
-
-        epsilon = tf.constant(value=1e-7)
-        logits_softmax = tf.nn.softmax(flat_logits) + epsilon
-
-        coef = alpha * tf.pow(1-logits_softmax, gamma)
-        focal_loss = -tf.reduce_sum(flat_labels*tf.log(logits_softmax)*coef, axis=1)
-
-    return tf.reduce_mean(focal_loss)
 
 if __name__ == '__main__':
-    labels = np.array([0, 1, 1, 2])
-    labels_onehot = to_categorical(labels)
-    logits = np.array([0.6, 0.2, 0.2, 0.4, 0.5, 0.1, 0.3, 0.6, 0.1, 0.05, 0.05, 0.9]).reshape([4, 3])
+    labels = np.array([0, 1, 2], dtype=np.float32)
+    labels_onehot = tf.keras.utils.to_categorical(labels)
+    logits = np.array([2.9, .1, .05, .3, 1.9, .1, .05, .05, 1.9],
+                      dtype=np.float32).reshape(-1, 3)
+    logits_softmax = tf.keras.layers.Softmax()(logits)
+    
+    print('labels(onehot)\n', labels_onehot)
+    print('logits(softmax)\n', logits_softmax)
 
-    fl = softmax_focal_loss(labels=labels_onehot, logits=logits, n_classes=3)
-    with tf.Session() as sess:
-        print(sess.run(fl))
+    print(categorical_cross_entropy(3)(labels_onehot, logits_softmax))
+    print(tf.keras.losses.CategoricalCrossentropy()(
+        labels_onehot, logits_softmax))
+    print(tf.keras.losses.categorical_crossentropy(
+        labels_onehot, logits_softmax))
--- unet.py
+++ unet.py
@@ -2,96 +2,85 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import Model, layers, optimizers
-import mydataset
 
-class unet(Model):
-    def __init__(self):
-        super(unet, self).__init__()
+def unet(inputs_shape):
+    
+    def _unet():
+        
+        inputs = layers.Input(shape=inputs_shape, name='inputs')
 
-        self.conv1_1 = layers.Conv2D(filters=64, kernel_size=(3, 3), padding='SAME', activation='relu', name='conv1_1')
-        self.conv1_2 = layers.Conv2D(filters=64, kernel_size=(3, 3), padding='SAME', activation='relu', name='conv1_2')
-        self.maxpool1 = layers.MaxPool2D(name='maxpool1')
+        conv1_1 = layers.Conv2D(64, 3, padding='SAME', activation='relu',
+                                name='conv1_1')(inputs)
+        conv1_2 = layers.Conv2D(64, 3, padding='SAME', activation='relu',
+                                name='conv1_2')(conv1_1)
+        maxpool1 = layers.MaxPool2D(name='maxpool1')(conv1_2)
 
-        self.conv2_1 = layers.Conv2D(filters=128, kernel_size=(3, 3), padding='SAME', activation='relu', name='conv2_1')
-        self.conv2_2 = layers.Conv2D(filters=128, kernel_size=(3, 3), padding='SAME', activation='relu', name='conv2_2')
-        self.maxpool2 = layers.MaxPool2D(name='maxpool2')
+        conv2_1 = layers.Conv2D(128, 3, padding='SAME', activation='relu',
+                                name='conv2_1')(maxpool1)
+        conv2_2 = layers.Conv2D(128, 3, padding='SAME', activation='relu',
+                                name='conv2_2')(conv2_1)
+        maxpool2 = layers.MaxPool2D(name='maxpool2')(conv2_2)
 
-        self.conv3_1 = layers.Conv2D(filters=256, kernel_size=(3, 3), padding='SAME', activation='relu', name='conv3_1')
-        self.conv3_2 = layers.Conv2D(filters=256, kernel_size=(3, 3), padding='SAME', activation='relu', name='conv3_2')
-        self.maxpool3 = layers.MaxPool2D(name='maxpool3')
+        conv3_1 = layers.Conv2D(256, 3, padding='SAME', activation='relu',
+                                name='conv3_1')(maxpool2)
+        conv3_2 = layers.Conv2D(256, 3, padding='SAME', activation='relu',
+                                name='conv3_2')(conv3_1)
+        maxpool3 = layers.MaxPool2D(name='maxpool3')(conv3_2)
 
-        self.conv4_1 = layers.Conv2D(filters=512, kernel_size=(3, 3), padding='SAME', activation='relu', name='conv4_1')
-        self.conv4_2 = layers.Conv2D(filters=512, kernel_size=(3, 3), padding='SAME', activation='relu', name='conv4_2')
-        self.maxpool4 = layers.MaxPool2D(name='maxpool4')
+        conv4_1 = layers.Conv2D(512, 3, padding='SAME', activation='relu',
+                                name='conv4_1')(maxpool3)
+        conv4_2 = layers.Conv2D(512, 3, padding='SAME', activation='relu',
+                                name='conv4_2')(conv4_1)
+        maxpool4 = layers.MaxPool2D(name='maxpool4')(conv4_2)
 
-        self.conv5_1 = layers.Conv2D(filters=1024, kernel_size=(3, 3), padding='SAME', activation='relu', name='conv5_1')
-        self.conv5_2 = layers.Conv2D(filters=1024, kernel_size=(3, 3), padding='SAME', activation='relu', name='conv5_2')
+        conv5_1 = layers.Conv2D(1024, 3, padding='SAME', activation='relu',
+                                name='conv5_1')(maxpool4)
+        conv5_2 = layers.Conv2D(1024, 3, padding='SAME', activation='relu',
+                                name='conv5_2')(conv5_1)
 
-        self.upsamping1 = layers.UpSampling2D(name='upsampling1')
-        self.upconv1 = layers.Conv2D(filters=512, kernel_size=(2, 2), padding='SAME', activation='relu', name='upconv1')
-        self.conv6_1 = layers.Conv2D(filters=512, kernel_size=(3, 3), padding='SAME', activation='relu', name='conv6_1')
-        self.conv6_2 = layers.Conv2D(filters=512, kernel_size=(3, 3), padding='SAME', activation='relu', name='conv6_2')
+        upsamping1 = layers.UpSampling2D(name='upsampling1')(conv5_2)
+        upconv1 = layers.Conv2D(512, 2, padding='SAME', activation='relu',
+                                name='upconv1')(upsamping1)
+        concat1 = layers.concatenate([conv4_2, upconv1], axis=3, name='concat1')
+        conv6_1 = layers.Conv2D(512, 3, padding='SAME', activation='relu',
+                                name='conv6_1')(concat1)
+        conv6_2 = layers.Conv2D(512, 3, padding='SAME', activation='relu',
+                                name='conv6_2')(conv6_1)
 
-        self.upsamping2 = layers.UpSampling2D(name='upsampling2')
-        self.upconv2 = layers.Conv2D(filters=256, kernel_size=(2, 2), padding='SAME', activation='relu', name='upconv2')
-        self.conv7_1 = layers.Conv2D(filters=256, kernel_size=(3, 3), padding='SAME', activation='relu', name='conv7_1')
-        self.conv7_2 = layers.Conv2D(filters=256, kernel_size=(3, 3), padding='SAME', activation='relu', name='conv7_2')
+        upsamping2 = layers.UpSampling2D(name='upsampling2')(conv6_2)
+        upconv2 = layers.Conv2D(256, 2, padding='SAME', activation='relu',
+                                name='upconv2')(upsamping2)
+        concat2 = layers.concatenate([conv3_2, upconv2], axis=3, name='concat2')
+        conv7_1 = layers.Conv2D(256, 3, padding='SAME', activation='relu',
+                                name='conv7_1')(concat2)
+        conv7_2 = layers.Conv2D(256, 3, padding='SAME', activation='relu',
+                                name='conv7_2')(conv7_1)
+    
+        upsamping3 = layers.UpSampling2D(name='upsampling3')(conv7_2)
+        upconv3 = layers.Conv2D(128, 2, padding='SAME', activation='relu',
+                                name='upconv3')(upsamping3)
+        concat3 = layers.concatenate([conv2_2, upconv3], axis=3, name='concat3')
+        conv8_1 = layers.Conv2D(128, 3, padding='SAME', activation='relu',
+                                name='conv8_1')(concat3)
+        conv8_2 = layers.Conv2D(128, 3, padding='SAME', activation='relu',
+                                name='conv8_2')(conv8_1)
 
-        self.upsamping3 = layers.UpSampling2D(name='upsampling3')
-        self.upconv3 = layers.Conv2D(filters=128, kernel_size=(2, 2), padding='SAME', activation='relu', name='upconv3')
-        self.conv8_1 = layers.Conv2D(filters=128, kernel_size=(3, 3), padding='SAME', activation='relu', name='conv8_1')
-        self.conv8_2 = layers.Conv2D(filters=128, kernel_size=(3, 3), padding='SAME', activation='relu', name='conv8_2')
+        upsamping4 = layers.UpSampling2D(name='upsampling4')(conv8_2)
+        upconv4 = layers.Conv2D(64, 2, padding='SAME', activation='relu',
+                                name='upconv4')(upsamping4)
+        concat4 = layers.concatenate([conv1_2, upconv4], axis=3, name='concat4')
+        conv9_1 = layers.Conv2D(64, 3, padding='SAME', activation='relu',
+                                name='conv9_1')(concat4)
+        conv9_2 = layers.Conv2D(64, 3, padding='SAME', activation='relu',
+                                name='conv9_2')(conv9_1)
 
-        self.upsamping4 = layers.UpSampling2D(name='upsampling4')
-        self.upconv4 = layers.Conv2D(filters=64, kernel_size=(2, 2), padding='SAME', activation='relu', name='upconv4')
-        self.conv9_1 = layers.Conv2D(filters=64, kernel_size=(3, 3), padding='SAME', activation='relu', name='conv9_1')
-        self.conv9_2 = layers.Conv2D(filters=64, kernel_size=(3, 3), padding='SAME', activation='relu', name='conv9_2')
-        self.conv9_3 = layers.Conv2D(filters=1, kernel_size=(1, 1), activation='relu', name='conv9_3')
+        outputs = layers.Conv2D(1, 1, activation='sigmoid',
+                                name='outputs')(conv9_2)
 
-    def call(self, inputs):
-        conv1 = self.conv1_1(inputs)
-        conv1 = self.conv1_2(conv1)
-        pool1 = self.maxpool1(conv1)
+        return Model(inputs=inputs, outputs=outputs)
+        
+    return _unet()
 
-        conv2 = self.conv2_1(pool1)
-        conv2 = self.conv2_2(conv2)
-        pool2 = self.maxpool2(conv2)
-
-        conv3 = self.conv3_1(pool2)
-        conv3 = self.conv3_2(conv3)
-        pool3 = self.maxpool3(conv3)
-
-        conv4 = self.conv4_1(pool3)
-        conv4 = self.conv4_2(conv4)
-        pool4 = self.maxpool4(conv4)
-
-        conv5 = self.conv5_1(pool4)
-        conv5 = self.conv5_2(conv5)
-
-        concat1 = layers.concatenate([conv4, self.upconv1(self.upsamping1(conv5))], axis=3, name='concat1')
-        conv6 = self.conv6_1(concat1)
-        conv6 = self.conv6_2(conv6)
-
-        concat2 = layers.concatenate([conv3, self.upconv2(self.upsamping2(conv6))], axis=3, name='concat2')
-        conv7 = self.conv7_1(concat2)
-        conv7 = self.conv7_2(conv7)
-
-        concat3 = layers.concatenate([conv2, self.upconv3(self.upsamping3(conv7))], axis=3, name='concat3')
-        conv8 = self.conv8_1(concat3)
-        conv8 = self.conv8_2(conv8)
-
-        concat4 = layers.concatenate([conv1, self.upconv4(self.upsamping4(conv8))], axis=3, name='concat4')
-        conv9 = self.conv9_1(concat4)
-        conv9 = self.conv9_2(conv9)
-        outputs = self.conv9_3(conv9)
-
-        return outputs
-
-train_folder = './data/myvoc/train'
-train_list_file_path = './data/myvoc/trainlist.txt'
-dataset = mydataset.mydataset(train_folder, train_list_file_path)
-images, labels = dataset.load()
-labels.reshape([20, 512, 512, 1])
-mynet = unet()
-mynet.compile(optimizer=optimizers.Adam(lr=1e-4), loss='binary_crossentropy', metrics=['accuracy'])
-mynet.fit(images, labels, batch_size=4, epochs=50)
+if __name__ == '__main__':
+    mynet = unet([512, 512, 3])
+    mynet.summary()
